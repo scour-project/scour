7500a58a5df14d85b934f2a049fc83739f0f6d08
JSCHILL1 <jschill1@Macintosh.local>
2009-04-02 14:32:54 -0500

    Added starter scour.py file and README

---
+++ README.txt
@@ -0,0 +1,17 @@
+Scour
+=====
+
+Scour will be a Python module that takes an input SVG and outputs a cleaner, 
+more concise SVG file.  The goal will be that authors will use this script
+after editing the file in a GUI editor such as Inkscape or Adobe Illustrator.
+
+Scour was started as a vehicle for me to learn Python.  In addition, the goal
+is to reduce the amount of time I spend in cleaning up files I find on sites
+such as openclipart.org
+
+Regards,
+
+Jeff Schiller
+2009-04-02
+codedread@gmail.com
+http://blog.codedread.com/
---
+++ scour.py
@@ -0,0 +1,22 @@
+#  Scour
+#  Version 0.01
+#
+#  Copyright 2009 Jeff Schiller
+#
+#  This file is part of Scour, http://www.codedread.com/scour/
+#
+#  Scour is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Scour is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Carve.  If not, see http://www.gnu.org/licenses/ .
+#
+
+print "Scour is here!"
