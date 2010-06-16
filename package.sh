fdabe0270166dfbe31ebad4ba65e49893f441ed8
Cynthia Gauthier <cynthia@jolteon>
2010-06-15 21:40:08 -0400

    Update push.sh with the new branch location for the trunk. Update package.sh, adding the newly-required file svg_transform.py.

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCOURVER="0.25"
 cd ..
-zip scour/tarballs/scour-$SCOURVER.zip scour/scour.py scour/yocto_css.py scour/svg_regex.py scour/LICENSE scour/NOTICE scour/README.txt scour/release-notes.html
+zip scour/tarballs/scour-$SCOURVER.zip scour/scour.py scour/yocto_css.py scour/svg_regex.py scour/svg_transform.py scour/LICENSE scour/NOTICE scour/README.txt scour/release-notes.html
 cd scour
-zip tarballs/scour-inkscape-extension-$SCOURVER.zip scour.inx scour.inkscape.py scour.py svg_regex.py scour/yocto_css.py
+zip tarballs/scour-inkscape-extension-$SCOURVER.zip scour.inx scour.inkscape.py scour.py svg_regex.py svg_transform.py scour/yocto_css.py
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Pushes updates from the trunk
-bzr push bzr+ssh://codedread@bazaar.launchpad.net/~codedread/scour/trunk
+bzr push bzr+ssh://codedread@bazaar.launchpad.net/~scouring/scour/trunk
 
 # Merge into our branch
 
