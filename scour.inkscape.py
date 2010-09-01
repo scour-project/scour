52e86032fa172e14c7317d79825add739cadac6e
Bernd Feige <627372@bugs.launchpad.net>
2010-08-31 22:10:58 -0400

    Future-proof the Scour Inkscape plugin's method of acquiring the name of the file to optimise. Thanks to Bernd Feige for the patch.

--- scour.inkscape.py
+++ scour.inkscape.py
@@ -57,7 +57,7 @@ class ScourInkscape (inkex.Effect):
             help="work around various renderer bugs (currently only librsvg)")
 
     def effect(self):   
-        input = file(sys.argv[12], "r")
+        input = file(self.args[0], "r")
         sys.stdout.write(scourString(input.read(), self.options).encode("UTF-8"))
         input.close()
         sys.stdout.close()
