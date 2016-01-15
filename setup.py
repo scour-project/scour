fed6203d3685e1839e2958b4c4197a17478152ee
Eitot <Eitot@users.noreply.github.com>
2016-01-15 03:22:02 +0100

    Fix import error in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 ##
 ###############################################################################
 
-import re
+from scour import __version__
 from setuptools import setup, find_packages
 
 LONGDESC = """
@@ -31,19 +31,9 @@ Authors:
   - Tobias Oberstein (maintainer)
 """
 
-VERSIONFILE = "scour/__init__.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = u['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
-
-
 setup (
    name = 'scour',
-   version = verstr,
+   version = __version__,
    description = 'Scour SVG Optimizer',
 #   long_description = open("README.md").read(),
    long_description = LONGDESC,
